ebaee303bc9973b5fd822e4d36cc96e7fbea74ee
QMHLO <htetlinoo@qualymm.com>
2023-06-27 11:02:18 +0630

    vercel production changed

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    brotliSize: false,
-    rollupOptions: {
-      output: {
-        format: "esm",
-        entryFileNames: "[name].js",
-        chunkFileNames: "chunks/[name].js",
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-  },
+  plugins: [react()],
 });
